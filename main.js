46e2686235d0a8c40ae346759c22f281e31504e9
dasuperhero <leong.kj.sg@gmail.com>
2024-05-28 19:15:59 +0800

    first save

--- main.js
+++ main.js
@@ -1,24 +1,2 @@
-// hover effect so divs change color when mouse pass over them
 
-
-//button at top of screen that sends user a popup asking for number of squares per side for the new grid
-//existing grid removed, new grid generated in the same total space
-let number = 16;
-const btn = document.querySelector("#btn");
-btn.addEventListener("click", () => {
-    number = parseInt(prompt("How many squares per side? (Maximum 100)"));
-    if (number > 100){
-        number = 100;
-    }
-})
-
-const container = document.querySelector("#container")
-function generategrid(number){
-    for(i = 0; i < number; i++){
-        for(j=0; j<number; j++){
-            const content = document.createElement("div");
-            container.appendChild(content);
-        }
-    }
-}
 
